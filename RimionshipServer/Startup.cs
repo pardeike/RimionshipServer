5a2e97430e9f4eba8fa76ed125dd02391457b9b3
Andreas Pardeike <andreas@pardeike.net>
2022-05-14 17:41:40 +0200

    adds twitch auth prototype (callback not working yet)

--- RimionshipServer/Startup.cs
+++ RimionshipServer/Startup.cs
@@ -1,9 +1,10 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using RimionshipServer.Services;
 
 namespace RimionshipServer
 {
@@ -20,19 +21,12 @@ namespace RimionshipServer
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
-			// _ = services.AddRazorPages();
-			// _ = services.AddServerSideBlazor();
-			// _ = services.AddControllersWithViews();
+			_ = services.AddRazorPages();
+			_ = services.AddServerSideBlazor();
+			_ = services.AddControllersWithViews();
 
-			/*
 			_ = services
 				.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-				// .AddAuthentication(options =>
-				// {
-				// 	options.DefaultAuthenticateScheme = TwitchAuthenticationDefaults.AuthenticationScheme;
-				// 	options.DefaultSignInScheme = TwitchAuthenticationDefaults.AuthenticationScheme;
-				// 	options.DefaultChallengeScheme = "Twitch";
-				// })
 				.AddCookie()
 				.AddTwitch(options =>
 				{
@@ -44,17 +38,12 @@ namespace RimionshipServer
 					options.ForceVerify = true;
 					options.SaveTokens = true;
 				});
-			*/
 
-			//_ = services.AddAuthorization();
+			_ = services.AddHttpClient();
+			_ = services.AddScoped<TokenProvider>();
+			_ = services.AddScoped<UserInfoService>();
 
-			// _ = services.AddHttpClient();
-			// _ = services.AddScoped<TokenProvider>();
-
-			_ = services.AddGrpc(options =>
-			{
-				options.EnableDetailedErrors = true;
-			});
+			_ = services.AddGrpc(options => options.EnableDetailedErrors = true);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,30 +52,20 @@ namespace RimionshipServer
 			if (env.IsDevelopment())
 				_ = app.UseDeveloperExceptionPage();
 			else
-			{
 				_ = app.UseExceptionHandler("/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				_ = app.UseHsts();
-			}
 
-			// _ = app.UseHttpsRedirection();
-			// _ = app.UseStaticFiles();
+			_ = app.UseStaticFiles();
 			_ = app.UseRouting();
-			// _ = app.UseAuthentication();
-			// _ = app.UseAuthorization();
+			_ = app.UseAuthentication();
+			_ = app.UseAuthorization();
 
 			_ = app.UseEndpoints(endpoints =>
 			{
 				_ = endpoints.MapGrpcService<GreeterService>();
 
-				//_ = endpoints.MapBlazorHub();
-				//_ = endpoints.MapFallbackToPage("/_Host");
-				//_ = endpoints.MapControllers();
-
-				_ = endpoints.MapGet("/", async context =>
-				{
-					await context.Response.WriteAsync("Use a gRPC client. Visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-				});
+				_ = endpoints.MapBlazorHub();
+				_ = endpoints.MapFallbackToPage("/_Host");
+				_ = endpoints.MapControllers();
 			});
 		}
 	}
