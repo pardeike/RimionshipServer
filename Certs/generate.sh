8f4d36502e6c0f799410364eb3551519f24a9933
Andreas Pardeike <andreas@pardeike.net>
2022-07-09 13:57:00 +0200

    removes fake cert generation

--- Certs/generate.sh
+++
@@ -1,4 +0,0 @@
-sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout server.key -out server.crt -config generate.conf
-sudo openssl pkcs12 -export -out server.pfx -inkey server.key -in server.crt
-
-cp server.pfx ../RimionshipServer
