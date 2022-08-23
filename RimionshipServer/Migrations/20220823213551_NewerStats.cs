6e8a8760c5d74948988ddbe99ca9bd79f031844d
Andreas Pardeike <andreas@pardeike.net>
2022-08-23 23:36:49 +0200

    adds newer stats migration

---
+++ RimionshipServer/Migrations/20220823213551_NewerStats.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RimionshipServer.Migrations
+{
+    public partial class NewerStats : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AmountBloodCleaned",
+                table: "LatestStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AnimalMeatCreated",
+                table: "LatestStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicksIgnoringBloodGod",
+                table: "LatestStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicksLowColonistMood",
+                table: "LatestStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AmountBloodCleaned",
+                table: "HistoryStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AnimalMeatCreated",
+                table: "HistoryStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicksIgnoringBloodGod",
+                table: "HistoryStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TicksLowColonistMood",
+                table: "HistoryStats",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AmountBloodCleaned",
+                table: "LatestStats");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalMeatCreated",
+                table: "LatestStats");
+
+            migrationBuilder.DropColumn(
+                name: "TicksIgnoringBloodGod",
+                table: "LatestStats");
+
+            migrationBuilder.DropColumn(
+                name: "TicksLowColonistMood",
+                table: "LatestStats");
+
+            migrationBuilder.DropColumn(
+                name: "AmountBloodCleaned",
+                table: "HistoryStats");
+
+            migrationBuilder.DropColumn(
+                name: "AnimalMeatCreated",
+                table: "HistoryStats");
+
+            migrationBuilder.DropColumn(
+                name: "TicksIgnoringBloodGod",
+                table: "HistoryStats");
+
+            migrationBuilder.DropColumn(
+                name: "TicksLowColonistMood",
+                table: "HistoryStats");
+        }
+    }
+}
