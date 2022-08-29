9bc896fbbf2da8e358ed0ff03cdd17bbe06e9d84
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-08-29 21:32:29 +0200

    Adds ChartJS & first simpleGraph test

---
+++ RimionshipServer/Pages/API/SimpleGraph.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RimionshipServer.Data;
+namespace RimionshipServer.Pages.Api;
+
+public class SimpleGraph : PageModel
+{
+    public record Dataset(string label, string backgroundColor, string borderColor, float[] data);
+    
+    public SimpleGraph(RimionDbContext dbContext)
+    {
+        _dbContext         = dbContext;
+    }
+
+    [BindProperty(SupportsGet = true)]
+    public string[] Labels { get; set; }
+        
+    [BindProperty(SupportsGet = true)]
+    public Dataset[] Datasets { get; set; }
+
+    private readonly RimionDbContext _dbContext;
+
+    public async Task<IActionResult> OnGet(string id, string secret)
+    {
+        if (id != "foo" || secret != "bar")
+        {
+            return NotFound();
+        }
+        Labels = new[]{
+                          "January",
+                          "February",
+                          "March",
+                          "April",
+                          "May",
+                          "June"
+                      };
+        Datasets = new[]{new Dataset("My First dataset", "rgb(255, 99, 132)", "rgb(255, 99, 132)", new float[]{0, 10, 5, 2, 20, 30, 45})};
+        return Page();
+    }
+}
