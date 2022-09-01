a713d74356b8f02ff36eaff74b30d2dca119cedf
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-09-01 23:04:52 +0200

    Forgot migrations

---
+++ RimionshipServer/Migrations/20220901205909_GraphAutoupdate.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RimionshipServer.Migrations
+{
+    public partial class GraphAutoupdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Autorefresh",
+                table: "GraphData",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Autorefresh",
+                table: "GraphData");
+        }
+    }
+}
