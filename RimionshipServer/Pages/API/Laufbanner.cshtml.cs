4f4c69b9ac0f4f6db667fba008653ffbba8f71d1
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-09-16 21:22:27 +0200

    Adjusted some visual things

---
+++ RimionshipServer/Pages/API/Laufbanner.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RimionshipServer.Data;
+
+namespace RimionshipServer.Pages.Api
+{
+    public class Laufbanner : PageModel
+    {
+        private readonly RimionDbContext _db;
+        public Laufbanner(RimionDbContext db)
+        {
+            _db = db;
+        }
+        
+        public IEnumerable<(int, string, double)> Entries { get; set; } = null!;
+        public async Task<IActionResult> OnGetAsync()
+        {
+            Entries = await Stats.GetTopXNotBannedUserFromDynamicCacheWithValue(nameof(HistoryStats.Wealth), int.MaxValue, _db);
+            return Page();
+        }
+    }
+}
