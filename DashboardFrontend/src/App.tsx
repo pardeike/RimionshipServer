a1b4f99b05a75065b2cefc836d893b07976ec079
Andreas Pardeike <andreas@pardeike.net>
2022-09-22 23:09:35 +0200

    fixes backlink

--- DashboardFrontend/src/App.tsx
+++ DashboardFrontend/src/App.tsx
@@ -27,9 +27,10 @@ const App: Component = () => {
   )
   onCleanup(() => clearInterval(interval))
 
-  const adminUrl = () => {
+  const gotoAdmin = () => {
     const hostname = window.location.host.replace('3000', '5062')
-    return `${window.location.protocol}//${hostname}/admin/`
+    window.location.assign(`${window.location.protocol}//${hostname}/admin/`)
+    return false
   }
 
   const currentDate = () => {
@@ -43,7 +44,7 @@ const App: Component = () => {
         <div>
           <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light border-bottom mb-3 p-0 position-relative">
             <span style="font-size: 18px; font-weight: 600" class="navbar-brand">Rimionship 2022</span>
-            <Link class="navbar-brand" href={adminUrl()}>Admin</Link>
+            <Link class="navbar-brand" href="#" onClick={() => gotoAdmin()}>Admin</Link>
             <Link class="navbar-brand" href="/">Dashboard</Link>
             <span class="position-absolute end-0" style="font-size: 18px; font-weight: 100; padding-right: 8px">{currentDate()}</span>
           </nav>
