3ccbb09d2a5a3f3be7a656596baf850b5a300648
Andreas Pardeike <andreas@pardeike.net>
2022-09-24 19:24:25 +0200

    fixes event sorting

--- RimionshipServer/Services/EventsService.cs
+++ RimionshipServer/Services/EventsService.cs
@@ -5,8 +5,8 @@ namespace RimionshipServer.Services
 {
     public class EventsService
     {
-        private object                   _lockobj = new();
-        public  ImmutableList<UserEvent> Events { get; private set; } = ImmutableList<UserEvent>.Empty;
+        private object _lockobj = new();
+        public ImmutableList<UserEvent> Events { get; private set; } = ImmutableList<UserEvent>.Empty;
 
         public void AddOrUpdateEvents(string userId, IEnumerable<UserEvent> events)
         {
@@ -15,7 +15,7 @@ namespace RimionshipServer.Services
                 var tmpList = Events.ToList();
                 tmpList.RemoveAll(evt => evt.UserId == userId);
                 tmpList.AddRange(events);
-                Events = tmpList.OrderByDescending(x => x.Ticks)
+                Events = tmpList.OrderBy(x => x.Ticks)
                                 .ToImmutableList();
             }
         }
