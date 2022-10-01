b5e7d2b314462d8b644151be9d60940a1b89a365
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-10-01 02:26:52 +0200

    HeadingTest

---
+++ RimionshipServer/Pages/API/SimpleGraphHeading.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RimionshipServer.Data;
+using RimionshipServer.Pages.Admin;
+
+namespace RimionshipServer.Pages.Api
+{
+    public class SimpleGraphHeading : PageModel
+    {
+        private readonly RimionDbContext _dbContext;
+        public SimpleGraphHeading(RimionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string GraphName { get; set; } = null!;
+
+        [BindProperty(SupportsGet = true)]
+        public string GraphHeading { get; set; } = null!;
+        
+        public async Task<IActionResult> OnGet(string secret)
+        {
+            GraphHeading = await _dbContext.GraphData
+                                           .AsNoTrackingWithIdentityResolution()
+                                           .Include(x => x.UsersReference)
+                                           .Where(x => x.Accesscode == secret)
+                                           .Select(x => x.Statt)
+                                           .FirstOrDefaultAsync();
+            
+            if (GraphHeading is null)
+            {
+                return NotFound();
+            }
+            GraphHeading = GraphConfigurator.StatsNames[GraphHeading];
+            return Page();
+        }
+    }
+}
