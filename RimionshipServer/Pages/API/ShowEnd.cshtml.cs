7d506d7d52c59b8c47b6b4c07286162c297b8b0a
bartimaeusnek <33183715+bartimaeusnek@users.noreply.github.com>
2022-10-02 21:46:58 +0200

    EndCSV export

---
+++ RimionshipServer/Pages/API/ShowEnd.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RimionshipServer.Data;
+
+namespace RimionshipServer.Pages.Api
+{
+    public class ShowEnd : PageModel
+    {
+        private readonly RimionDbContext _dbContext;
+        public ShowEnd(RimionDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        
+        public async Task<IActionResult> OnGet()
+        {
+            var wealth = await Stats.GetTopXNotBannedUserFromDynamicCacheWithValue(nameof(Stats.Wealth), Int32.MaxValue, _dbContext);
+            var ret = wealth.OrderBy(x => x.Item3).Aggregate("Name;Koloniewert;\n", (y, x) =>  y + $"{x.Item2};{x.Item3};\n");
+            return new OkObjectResult(ret);
+        }
+    }
+}
